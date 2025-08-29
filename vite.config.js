8fcacc2614d56b38d056d336e6ccab250e494187
Florian <florian.glay@student.junia.com>
2025-08-29 23:54:12 +0200

    Name changed

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   // base = racine en dev, sous-chemin en prod (GitHub Pages)
-  base: process.env.NODE_ENV === "production" ? "/k4teri-web-cv/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/portfolio/" : "/",
 });
